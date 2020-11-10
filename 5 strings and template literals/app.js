1304f1337c7c30b379cac50ed3972bcc662ffb55
sam <sammybonfanti@gmail.com>
2020-11-10 16:11:45 -0700

    done

---
+++ 5 strings and template literals/app.js
@@ -0,0 +1,8 @@
+const country = 'USA'
+const continent = 'North America'
+let population = '328.2'
+const isIsland = 'USA'
+const language = 'English'
+
+const description = `${country} is in ${continent}, and its ${population} million pople speak ${language}`
+console.log(description);
