d73760965db935af184e7b5fbc7c39e1a92a73de
sam <sammybonfanti@gmail.com>
2020-11-10 15:34:41 -0700

    done

---
+++ 1 values and variables/app.js
@@ -0,0 +1,5 @@
+const country = 'USA'
+const continent = 'North America'
+const population = '328.2 million'
+
+console.log(`The ${country} is in ${continent} and it has a population of ${population}`);
