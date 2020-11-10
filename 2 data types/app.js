552cb7ce2ba771b0d75708c491acb4eb01cd9f5a
sam <sammybonfanti@gmail.com>
2020-11-10 15:44:49 -0700

    done

---
+++ 2 data types/app.js
@@ -0,0 +1,10 @@
+const country = 'USA'
+const continent = 'North America'
+const population = '328.2 million'
+const isIsland = 'USA'
+let language
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
