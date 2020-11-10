1913b6c807ee7c43cced4bc204c68e94b2178a79
sam <sammybonfanti@gmail.com>
2020-11-10 16:09:26 -0700

    done

---
+++ 4 basic operators/app.js
@@ -0,0 +1,13 @@
+const country = 'USA'
+const continent = 'North America'
+let population = '328.2'
+const isIsland = 'USA'
+const language = 'English'
+
+console.log(population / 2);
+population++
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description = `${country} is in ${continent}, and its ${population} million pople speak ${language}`
+console.log(description);
