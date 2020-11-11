58935fc9538f4c489d3a6e11cfb44acb38241f94
sam <sammybonfanti@gmail.com>
2020-11-10 18:17:57 -0700

    done

---
+++ 11 the conditional ternary operator/app.js
@@ -0,0 +1,7 @@
+const country = 'USA'
+const continent = 'North America'
+let population = 13
+const isIsland = false
+const language = 'english'
+
+console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} avarage`);
